f53e734034daffd694871c4b74e7f6de995d8824
Florian Mold <florian.mold@live.at>
2022-07-09 11:04:14 +0200

    Synching history objects

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -8,6 +8,7 @@
  */
 import {mount} from 'marketing/MarketingApp';
 import React, {useRef, useEffect} from 'react';
+import {useHistory} from "react-router-dom";
 
 // With useRef we can create a reference to an HTML-Element.
 // This is where we want to render our marketing element into.
@@ -15,14 +16,26 @@ import React, {useRef, useEffect} from 'react';
 export default () => {
     /** This is the reference to the html-element displayed on the screen. */
     const ref = useRef(null);
+    /** A copy of the browser-history used in our container. */
+    const history = useHistory();
 
     // The useEffect makes sure, that we can run the code only one time, when the component is being created.
     useEffect(() => {
         // Mount takes the reference of the div and tries to create the marketing app.
         mount(ref.current, {
-            // We pass a callback as well, that is executed by the marketing app, when navigation takes place.
-            onNavigate: () => {
-                console.log('The container noticed navigation in Marketing');
+            // We pass a callback, that is executed by the marketing app, when navigation takes place.
+            // The location is what is communicated from the marketing-router to the container.
+            // pathname is the path, where the marketing app navigates to.
+            onNavigate: ({pathname: nextPathname}) => {
+                const {pathname} = history.location;
+
+                /**
+                 * Only navigate, when the current pathname and the next-pathname are different.
+                 */
+                if (pathname !== nextPathname) {
+                    /** This means, that we want to navigate to the given path-name. */
+                    history.push(nextPathname);
+                }
             }
         });
     });
